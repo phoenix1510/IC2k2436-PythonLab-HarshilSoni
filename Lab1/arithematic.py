4746e16b9efaa856aa53215ad4817a2ae70d64ca
pheonix1510 <soniharshil.1510@gmail.com>
2026-08-14 09:12:35 +0530

    Arithematic_task

---
+++ Lab1/arithematic.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter Second number: "))
+print(f"Addition = {num1+num2}")
+print(f"Subtraction = {num1-num2}")
+print(f"Multiplication = {num1*num2}")
+print(f"Division = {num1/num2}")
