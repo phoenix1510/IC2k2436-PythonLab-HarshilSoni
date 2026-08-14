83dc6a3847c3b5ad2f334665b975d74e4dca8034
pheonix1510 <soniharshil.1510@gmail.com>
2026-08-14 09:49:07 +0530

    forgot to add comment at the top

--- Lab1/escape_sequence.py
+++ Lab1/escape_sequence.py
@@ -1,3 +1,4 @@
+"""Write a short program that prints a small receipt or table like output using \t and \n to align text neatly, for example an item name and its price on each line."""
 print("Item\t\tPrice")
 print("Milk\t\t₹60")
 print("Bread\t\t₹40")
